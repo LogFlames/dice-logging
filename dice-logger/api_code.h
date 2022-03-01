086a23c084df4520233ded9182321375efab3dce
Elias Lundell <36220731+LogFlames@users.noreply.github.com>
2022-03-01 10:06:35 +0100

    Had to generate new API code :(

--- dice-logger/api_code.h
+++ dice-logger/api_code.h
@@ -1,4 +1,4 @@
-String api_code = "MWMwZGQxODUtMGQ5MC00ODQ4LTgzMjAtYTIwYTA1MTY3ZTg0";
+String api_code = "<API CODE>";
 std::vector<String> workspace_names = {"<WORKSPACE NAME 1>", "<WORKSPACE NAME 2>"};
 std::vector<String> workspace_ids = {"<WORKSPACE ID 1>", "<WORKSPACE ID 2>"};
 
