d59cb3946f08c8d41e0c43d9ff4e59beeb3ab305
Elias Lundell <elilun03@gmail.com>
2022-03-01 20:55:53 +0100

    Integrate functions since I because of another error through this was wrong, it wasn't, but it has now changed

--- dice-logger/api_code.h
+++ dice-logger/api_code.h
@@ -1,3 +1,12 @@
 String api_code = "<API CODE>";
-std::vector<String> workspace_names = {"<WORKSPACE NAME 1>", "<WORKSPACE NAME 2>"};
-std::vector<String> workspace_ids = {"<WORKSPACE ID 1>", "<WORKSPACE ID 2>"};
+
+std::vector<String> workspace_names;
+std::vector<String> workspace_ids;
+
+void setup_workspaces() {
+    workspace_names.push_back("<WORKSPACE NAME 1>");
+    workspace_ids.push_back("<WORKSPACE ID 1>");
+
+    workspace_names.push_back("<WORKSPACE NAME 2>");
+    workspace_ids.push_back("<WORKSPACE ID 2>");
+}
